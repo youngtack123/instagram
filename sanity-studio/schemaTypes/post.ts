90682706748ad43c5e126548f7308b52739f97bf
youngtack123 <youngtack123@gmail.com>
2024-03-07 20:33:53 +0900

    :white_check_mark: Add schema for Post
    Add schema for Post

--- sanity-studio/schemaTypes/post.ts
+++ sanity-studio/schemaTypes/post.ts
@@ -1,1 +1,53 @@
-export default {}
+import {Rule} from 'sanity'
+
+export default {
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    {
+      title: 'Author',
+      name: 'author',
+      type: 'reference',
+      to: [{type: 'user'}],
+    },
+    {
+      title: 'Photo',
+      name: 'photo',
+      type: 'image',
+    },
+    {
+      title: 'Likes',
+      name: 'likes',
+      type: 'array',
+      of: [{type: 'user', to: [{type: 'user'}]}],
+      validation: (Rule: Rule) => Rule.unique(),
+    },
+    {
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
+        {
+          title: 'Comment',
+          name: 'comment',
+          type: 'document',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'user'}],
+            },
+            {
+              title: 'Comment',
+              name: 'comment',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+      validation: (Rule: Rule) => Rule.unique(),
+    },
+  ],
+}
