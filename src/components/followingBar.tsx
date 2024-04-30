039364ffc488b149d6350041f5842f1884475d6f
youngtack123 <youngtack123@gmail.com>
2024-04-30 21:39:13 +0900

    ✨ Apply GROQ query
    apply GROQ query

--- src/components/followingBar.tsx
+++ src/components/followingBar.tsx
@@ -3,13 +3,8 @@
 import useSWR from "swr";
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR("/api/hello");
+  const { data, isLoading, error } = useSWR("/api/me");
   console.log(data);
-  // 1. 클라이언트 컴포넌트에서 백엔드에게 api/me 사용자의 정보를 얻어옴
-
-  // 2. 백엔드에서는 현재 로그인된 사용자의 세션 정보를 이용해서
-
-  // 3. 백엔드에서 사용자의 상세 정보를 Sanity에서 가지고옴(followings)
 
   // 4. 여기에서, 클라이언트 컴포넌트에서 followings의 정보를 UI에 보여줌
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -24,7 +24,6 @@ export const authOptions: NextAuthOptions = {
       return true;
     },
     async session({ session }) {
-      console.log(session);
       const user = session?.user;
       if (user) {
         session.user = {
