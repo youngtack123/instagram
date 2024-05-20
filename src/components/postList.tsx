b9930b688109ff83c0d335abb31a9a9f8d588aef
youngtack123 <youngtack123@gmail.com>
2024-05-20 11:02:54 +0900

    ✨ Implement PostList service
    implement PostList service

--- src/components/postList.tsx
+++ src/components/postList.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+"use client";
+import { SimplePost } from "@/model/post";
+import useSWR from "swr";
 
 export default function PostList() {
-  return <div>PostList</div>;
+  const { data: posts, isLoading: loading } =
+    useSWR<SimplePost[]>("/api/posts");
+
+  return (
+    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+  );
 }
---
+++ src/model/post.ts
@@ -0,0 +1,19 @@
+export type Comment = {
+  comment: string;
+  username: string;
+  image: string;
+};
+
+export type SimplePost = Omit<FullPost, "comments"> & {
+  comments: number;
+};
+
+export type FullPost = {
+  id: string;
+  username: string;
+  image: string;
+  text: string;
+  createdAt: string;
+  likes: string[];
+  comments: Comment[];
+};
