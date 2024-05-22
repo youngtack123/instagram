742c22ee2b543d49c04ebc4fda53db4380450721
youngtack123 <youngtack123@gmail.com>
2024-05-22 11:47:01 +0900

    ⚡️ Refactor PostList component and use dynamic import GridLoader
    refactor PostList component and use dynamic import GridLoader

--- src/components/postList.tsx
+++ src/components/postList.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { SimplePost } from "@/model/post";
-import { GridLoader } from "react-spinners";
 import useSWR from "swr";
 import PostListCard from "./postListCard";
+import GridSpinner from "./ui/gridSpinner";
 
 export default function PostList() {
   const { data: posts, isLoading: loading } =
@@ -12,15 +12,15 @@ export default function PostList() {
     <section>
       {loading && (
         <div className="text-center mt-32">
-          <GridLoader color="red" />
+          <GridSpinner color="red" />
         </div>
       )}
 
       {posts && (
         <ul>
-          {posts.map((post) => (
+          {posts.map((post, index) => (
             <li key={post.id} className="mb-4">
-              <PostListCard post={post} />
+              <PostListCard post={post} priority={index < 2} />
             </li>
           ))}
         </ul>
--- src/components/postListCard.tsx
+++ src/components/postListCard.tsx
@@ -1,17 +1,16 @@
 import { SimplePost } from "@/model/post";
-import React from "react";
 import Avartar from "./avartar";
 import Image from "next/image";
-import HeartIcon from "./ui/icons/HeartIcon";
-import BookmarkIcon from "./ui/icons/BookmarkIcon";
-import { parseDate } from "@/utils/date";
-import SmileIcon from "./ui/icons/SmileIcon";
+
+import CommentForm from "./commentForm";
+import ActionBar from "./actionBar";
 
 type Props = {
   post: SimplePost;
+  priority?: boolean;
 };
 
-export default function PostListCard({ post }: Props) {
+export default function PostListCard({ post, priority = false }: Props) {
   const { userImage, username, image, createdAt, likes, text } = post;
 
   return (
@@ -20,40 +19,21 @@ export default function PostListCard({ post }: Props) {
         <Avartar image={userImage} size="medium" highlight />
         <span className="text-gray-900 font-bold ml-2">{username}</span>
       </div>
-
       <Image
         className="w-full object-cover aspect-square"
         src={image}
         alt={`photo by ${username}`}
         width={500}
         height={500}
+        priority={priority}
       />
-
-      <div className="flex justify-between my-2 px-4">
-        <HeartIcon />
-        <BookmarkIcon />
-      </div>
-      <div className="px-4 py-1">
-        <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${
-          likes?.length > 1 ? "likes" : "like"
-        }`}</p>
-        <p>
-          <span className="font-bold mr-1">{username}</span>
-          {text}
-        </p>
-        <p className="text-xs text-neutral-500 uppercase my-2">
-          {parseDate(createdAt)}
-        </p>
-        <form className="flex items-center border-t border-neutral-300">
-          <SmileIcon />
-          <input
-            className="w-full ml-2 border-none outline-none p-3"
-            type="text"
-            placeholder="Add a comment..."
-          />
-          <button className="font-bold text-sky-500 ml-2">Post</button>
-        </form>
-      </div>
+      <ActionBar
+        likes={likes}
+        username={username}
+        text={text}
+        createdAt={createdAt}
+      />
+      <CommentForm />
     </article>
   );
 }
