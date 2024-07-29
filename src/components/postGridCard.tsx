419afea8e659ba2df38453673729c2737816b924
youngtack123 <youngtack123@gmail.com>
2024-07-29 10:18:55 +0900

    💄 Style userPosts page and related components
    style userPosts page and related components

--- src/components/postGridCard.tsx
+++ src/components/postGridCard.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { SimplePost } from "@/model/post";
 import Image from "next/image";
 import { useState } from "react";
 import ModalPortal from "./ui/modalPortal";
 import PostModal from "./postModal";
 import PostDetail from "./postDetail";
+import { signIn, useSession } from "next-auth/react";
 
 type Props = {
   post: SimplePost;
@@ -13,15 +15,24 @@ type Props = {
 export default function PostGridCard({ post, priority = false }: Props) {
   const [openModal, setOpenModal] = useState(false);
   const { image, username } = post;
+  const { data: session } = useSession();
+  const handleOpenPost = () => {
+    if (!session?.user) {
+      return signIn();
+    }
+    setOpenModal(true);
+  };
 
   return (
-    <div>
+    <div className="relative w-full aspect-square">
       <Image
+        className="object-cover"
         src={image}
         alt={`photo by ${username}`}
         fill
         sizes="650px"
         priority={priority}
+        onClick={handleOpenPost}
       />
 
       {openModal && (
