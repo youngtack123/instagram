2017eec7d7e1ff30a472b4fb7d241afc115b83f4
youngtack123 <youngtack123@gmail.com>
2024-08-24 17:10:48 +0900

    🐛 Handle SSR page not updating after data request from client-component
    handle SSR page not updating after data request from client-component by using router.refresh()

--- src/components/followButton.tsx
+++ src/components/followButton.tsx
@@ -2,6 +2,9 @@
 import { ProfileUser } from "@/model/user";
 import Button from "./ui/button";
 import useMe from "@/hooks/useMe";
+import { useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { PulseLoader } from "react-spinners";
 
 type Props = {
   user: ProfileUser;
@@ -9,7 +12,11 @@ type Props = {
 
 export default function FollowButton({ user }: Props) {
   const { username } = user;
-  const { user: loggedInUser } = useMe();
+  const { user: loggedInUser, toggleFollow } = useMe();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
 
   const showButton = loggedInUser && loggedInUser.username !== username;
   const following =
@@ -20,10 +27,31 @@ export default function FollowButton({ user }: Props) {
 
   const text = following ? "Unfollow" : "Follow";
 
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !following);
+    setIsFetching(false);
+    startTransition(() => {
+      router.refresh();
+    });
+  };
+
   return (
     <>
       {showButton && (
-        <Button text={text} onClick={() => {}} red={text === "Unfollow"} />
+        <div className="relative">
+          {isUpdating && (
+            <div className="absolute z-20 inset-0 flex justify-center items-center">
+              <PulseLoader size={6} />
+            </div>
+          )}
+          <Button
+            disabled={isUpdating}
+            text={text}
+            onClick={handleFollow}
+            red={text === "Unfollow"}
+          />
+        </div>
       )}
     </>
   );
--- src/service/user.ts
+++ src/service/user.ts
@@ -62,13 +62,14 @@ export async function getUserForProfile(username: string) {
   return client
     .fetch(
       `*[_type == "user" && username == "${username}"][0]{
-      ...,
-      "id": _id,
-      "following": count(following),
-      "followers": count(followers),
-      "posts": count(*[_type == "post" && author->username == "${username}"])
-    }
-    `
+        ...,
+        "id": _id,
+        "following": count(following),
+        "followers": count(followers),
+        "posts": count(*[_type == "post" && author->username == "${username}"])
+      }`,
+      undefined,
+      { cache: "no-store" }
     )
     .then((user) => ({
       ...user,
