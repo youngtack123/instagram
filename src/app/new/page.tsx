313ae991a35388d351f7e7bb6d0457c61a45bd3e
youngtack123 <youngtack123@gmail.com>
2024-08-29 11:33:11 +0900

    ✨ Implement drag & drop feature in New page
    implement drag & drop feature in New page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import NewPost from "@/components/NewPost";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-function NewPostPage() {
-  return <div>NewPostPage</div>;
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new post",
+};
+
+export default async function NewPostPage() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    redirect("/auth/signin");
+  }
+
+  return <NewPost user={session.user} />;
 }
-
-export default NewPostPage;
---
+++ src/components/NewPost.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { AuthUser } from "@/model/user";
+import PostUserAvatar from "./PostUserAvatar";
+import FilesIcon from "./ui/icons/FilesIcon";
+import Button from "./ui/button";
+import { ChangeEvent, DragEvent, useState } from "react";
+
+type Props = {
+  user: AuthUser;
+};
+
+export default function NewPost({ user: { username, image } }: Props) {
+  const [dragging, setDragging] = useState(false);
+  const [file, setFile] = useState<File>();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const files = e.target?.files;
+
+    if (files && files[0]) {
+      setFile(files[0]);
+      console.log(files[0]);
+    }
+  };
+  const handleDrag = (e: DragEvent) => {
+    if (e.type === "dragenter") {
+      setDragging(true);
+    } else if (e.type === "dragleave") {
+      setDragging(false);
+    }
+  };
+  const handleDragOver = (e: DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: DragEvent) => {
+    e.preventDefault();
+    setDragging(false);
+    const files = e.dataTransfer?.files;
+
+    if (files && files[0]) {
+      setFile(files[0]);
+      console.log(files[0]);
+    }
+  };
+
+  return (
+    <section>
+      <PostUserAvatar username={username} image={image ?? ""} />
+      <form>
+        <input
+          className="hidden"
+          name="input"
+          id="input-upload"
+          type="file"
+          accept="image/*"
+          onChange={handleChange}
+        />
+        <label
+          htmlFor="input-upload"
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        >
+          <FilesIcon />
+          <p>Drag and Drop your image here or click</p>
+        </label>
+        <textarea
+          name="text"
+          id="input-text"
+          required
+          rows={10}
+          placeholder={`Write a caption...`}
+        />
+        <Button text="Publish" onClick={() => {}} />
+      </form>
+    </section>
+  );
+}
