35a2df39d90862be5d0a09bb23a38efed20f2f41
youngtack123 <youngtack123@gmail.com>
2024-08-29 12:25:04 +0900

    💄 Style NewPost component: Image upload UI styling
    style NewPost component: Image upload UI styling

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -4,6 +4,7 @@ import PostUserAvatar from "./PostUserAvatar";
 import FilesIcon from "./ui/icons/FilesIcon";
 import Button from "./ui/button";
 import { ChangeEvent, DragEvent, useState } from "react";
+import Image from "next/image";
 
 type Props = {
   user: AuthUser;
@@ -45,9 +46,9 @@ export default function NewPost({ user: { username, image } }: Props) {
   };
 
   return (
-    <section>
+    <section className="w-full max-w-xl flex flex-col items-center mt-6">
       <PostUserAvatar username={username} image={image ?? ""} />
-      <form>
+      <form className="w-full flex flex-col mt-2">
         <input
           className="hidden"
           name="input"
@@ -57,16 +58,38 @@ export default function NewPost({ user: { username, image } }: Props) {
           onChange={handleChange}
         />
         <label
+          className={`w-full h-60 flex flex-col items-center justify-center ${
+            !file && "border-2 border-sky-500 border-dashed"
+          }`}
           htmlFor="input-upload"
           onDragEnter={handleDrag}
           onDragLeave={handleDrag}
           onDragOver={handleDragOver}
           onDrop={handleDrop}
         >
-          <FilesIcon />
-          <p>Drag and Drop your image here or click</p>
+          {dragging && (
+            <div className="absolute inset-0 z-10 bg-sky-500/20 pointer-events-none"></div>
+          )}
+          {!file && (
+            <div className="flex flex-col items-center pointer-events-none">
+              <FilesIcon />
+              <p>Drag & Drop your image here or click</p>
+            </div>
+          )}
+          {file && (
+            <div className="relative w-full aspect-square">
+              <Image
+                className="object-cover"
+                src={URL.createObjectURL(file)}
+                alt="local file"
+                fill
+                sizes="650px"
+              />
+            </div>
+          )}
         </label>
         <textarea
+          className="outline-none text-lg border border-neutral-300 p-2"
           name="text"
           id="input-text"
           required
